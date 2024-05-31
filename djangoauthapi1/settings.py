d2e526dc467bd9105194b7239276cbad2163823c
Vivek Choudhary <vivek.choudhary.0022@gmail.com>
2024-05-31 10:42:23 +0530

    vercel.json update

--- djangoauthapi1/settings.py
+++ djangoauthapi1/settings.py
@@ -182,5 +182,5 @@ PASSWORD_RESET_TIMEOUT=900          # 900 Sec = 15 Min
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    "https://djangoweb-psi.vercel.app"
+    "djangoweb-psi.vercel.app"
 ]
