1254fb1287a0e74dc7e6407d8c7ed2e826ea7f9b
Vivek Choudhary <vivek.choudhary.0022@gmail.com>
2024-06-03 13:06:14 +0530

    static-file

--- djangoauthapi1/urls.py
+++ djangoauthapi1/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import index
+# from .views import index
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    re_path(r'^(?:.*)/?$', index),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/user/', include('account.urls')),
     path("api-auth/",include("rest_framework.urls")),
