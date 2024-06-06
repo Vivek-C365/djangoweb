c8d03ce2c7c1669d6bacaa3d1618d83862aac661
Vivek Choudhary <vivek.choudhary.0022@gmail.com>
2024-06-06 16:14:30 +0530

    test

--- djangoauthapi1/urls.py
+++ djangoauthapi1/urls.py
@@ -1,19 +1,36 @@
+# myproject/urls.py
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf import settings
-# from .views import index
 from django.views.generic import TemplateView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+# Swagger schema view configuration
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/user/', include('account.urls')),
-    path("api-auth/",include("rest_framework.urls")),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
+    path("api-auth/", include("rest_framework.urls")),
+    
+    # Add the following line for Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
